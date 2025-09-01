681dc3378ac2c67396b3ac94f63d8a55e8784533
469138946ba5fa <206320576+469138946ba5fa@users.noreply.github.com>
2025-09-01 12:01:57 +0800

    Update make-sing-box-env.sh
    修改DNS配置描述和转发参数

--- make-sing-box-env.sh
+++ make-sing-box-env.sh
@@ -316,15 +316,14 @@ pf_nat_udp_tcp() {
 
   # 写入 anchor 规则
   cat <<469138946ba5fa_1 | sudo tee \$ANCHOR_FILE
-# NAT 出口伪装，保证 Mac 自身流量出外网正常
+# NAT 所有来自局域网的流量
 nat on \$IFACE from any to any -> (\$IFACE)
 
-# 跳过本机和局域网流量
-no rdr on \$IFACE proto {tcp udp} from any to 127.0.0.1
-no rdr on \$IFACE proto {tcp udp} from any to {192.168.0.0/16 10.0.0.0/8 172.16.0.0/12}
+# DNS 劫持：所有设备的 DNS 请求转发到 TUN 网卡
+rdr pass on \$IFACE proto udp from any to any port 53 -> 172.19.0.1 port 53
 
-# 全局转发到 sing-box TUN
-rdr pass on \$IFACE proto {tcp udp} from any to any -> 172.19.0.1
+# TCP/UDP 流量转发到 sing-box
+rdr pass on \$IFACE proto {tcp udp} from any to any -> 172.19.0.1 port 7890
 469138946ba5fa_1
 
   # 重载 PF 加载并启用 PF
@@ -358,8 +357,8 @@ IFS=\$IFS_BAK
 chmod -v a+x ${SING_BOX_START}
 echo "已生成启动脚本: ${SING_BOX_START}"
 
-echo "如果想要全局路由你需要配置路由器 DHCP 下发的 NetGateway 强制为本机 IP 同时将下发 DNS 强制修改为 172.19.0.1 后执行脚本"
-echo "如果想要旁路由，你需要为单个联网设备配置 NetGateway 强制为本机 IP 同时将下发 DNS 强制修改为 172.19.0.1 后执行脚本"
+echo "如果想要全局路由你需要配置路由器 DHCP 下发的 NetGateway 强制为本机 IP 同时将下发 DNS 修改为任意真实 DNS（如 8.8.8.8 或 223.5.5.5），不要设置为 172.19.0.1 或 fake-ip 地址"
+echo "如果想要旁路由，你需要为单个联网设备配置 NetGateway 强制为本机 IP 同时将下发 DNS 修改为任意真实 DNS（如 8.8.8.8 或 223.5.5.5），不要设置为 172.19.0.1 或 fake-ip 地址"
 echo "如果想要端口代理，你需要将联网代理设置为本机 IP:7890"
 echo "如果想要本机，那就什么都没什么可说的了"
 echo "执行脚本 ${SING_BOX_START} 启动测试看看吧"
