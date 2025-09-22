7d1f87a11e91d36f23465450d5e09671ea9e79bf
469138946ba5fa <206320576+469138946ba5fa@users.noreply.github.com>
2025-09-23 00:04:30 +0800

    Update make-sing-box-env.sh
    开启pf

--- make-sing-box-env.sh
+++ make-sing-box-env.sh
@@ -338,17 +338,18 @@ pf_nat_udp_tcp() {
   # 写入 anchor 规则
   cat <<469138946ba5fa_1 | sudo tee \$ANCHOR_FILE
 # NAT 出口伪装，保证 Mac 自身流量出外网正常
-#nat on \$IFACE from any to any -> (\$IFACE)
+nat on \$IFACE from any to any -> ($IFACE)
 
 # DNS 劫持（如果设备没手动设置 DNS）
-#rdr pass on \$IFACE proto udp from any to any port 53 -> 172.19.0.1 port 53
+#rdr pass on \$IFACE proto {tcp udp} from any to any port 53 -> 172.19.0.1 port 53
+rdr pass on \$IFACE proto udp from any to any port 53 -> 172.19.0.1 port 53
 
 # TCP/UDP 流量转发到 sing-box 7890
 #rdr pass on \$IFACE proto {tcp udp} from any to any -> 172.19.0.1 port 7890
 # TCP 流量转发到 sing-box 7890
 #rdr pass on \$IFACE proto tcp from any to any -> 172.19.0.1 port 7890
 # TCP/UDP 流量转发到 sing-box TUN
-#rdr pass on \$IFACE proto {tcp udp} from any to any -> 172.19.0.1
+rdr pass on \$IFACE proto {tcp udp} from any to any -> 172.19.0.1
 # TCP 流量转发到 sing-box TUN
 #rdr pass on \$IFACE proto tcp from any to any -> 172.19.0.1
 469138946ba5fa_1
