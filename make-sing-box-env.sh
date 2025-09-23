8edd89c7385eae06fb9d07c26a3467430a334c1d
469138946ba5fa <206320576+469138946ba5fa@users.noreply.github.com>
2025-09-23 22:52:05 +0800

    Update make-sing-box-env.sh
    pf拦截无效

--- make-sing-box-env.sh
+++ make-sing-box-env.sh
@@ -342,10 +342,10 @@ nat on \$IFACE from any to any -> ($IFACE)
 
 # DNS 劫持（如果设备没手动设置 DNS）
 #rdr pass on \$IFACE proto {tcp udp} from any to any port 53 -> 172.19.0.1 port 53
-rdr pass on \$IFACE proto udp from any to any port 53 -> 172.19.0.1 port 53
+#rdr pass on \$IFACE proto udp from any to any port 53 -> 172.19.0.1 port 53
 
 # 阻断 DoT
-block out on \$IFACE proto tcp from any to any port 853
+#block out on \$IFACE proto tcp from any to any port 853
 
 # （可选）阻断常见 DoH IP
 #table <doh> persist { 1.1.1.1, 1.0.0.1, 8.8.8.8, 8.8.4.4 }
@@ -357,7 +357,7 @@ block out on \$IFACE proto tcp from any to any port 853
 # TCP 流量转发到 sing-box 7890
 #rdr pass on \$IFACE proto tcp from any to any -> 172.19.0.1 port 7890
 # TCP/UDP 流量转发到 sing-box TUN
-rdr pass on \$IFACE proto {tcp udp} from any to any -> 172.19.0.1
+#rdr pass on \$IFACE proto {tcp udp} from any to any -> 172.19.0.1
 # TCP 流量转发到 sing-box TUN
 #rdr pass on \$IFACE proto tcp from any to any -> 172.19.0.1
 469138946ba5fa_1
